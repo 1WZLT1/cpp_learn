174802d37908234e891c939729a8e63ba35cc236
1WZLT1 <1471147933@qq.com>
2024-12-25 17:41:07 +0800

    增加了new数组部分的代码，新学了一个东西，就是释放内存的具体含义是把这块内存标记成可用，像copy的那种操作是及其危险的，因为你仔细思考我copy指针虽然还可以继续访问使用那块内存，但是一点new又分配到别的变量的话，这种非法操作可能还会出现变量赋值不对的问题。

--- new_learn.cpp
+++ new_learn.cpp
@@ -2,21 +2,42 @@
 using namespace std;
 
 //堆区程序员手动释放 
-//new    返回的是地址
-//delete 相当于free操作
+//new             返回的是地址
+//delete          相当于free操作
+//new    关键字[] 创建数组
+//delete []       释放数组
 
-int *function()
+int *function_variable()
 {
     int *p = new int(10);
     return p;
 }
 
+int *function_array()
+{
+    int *p = new int[10];
+    return p;
+}
+
 void test_01()
 {
-    int *p = function();
-    cout << *p << endl;
+    int *p   = function_variable();
+    int *p_a = function_array(); 
+
+    for(int i = 0;i<10;i++)
+    p_a[1] = 1;  
+    cout << p_a[1] << endl;
+    
     delete(p);
     cout << *p << endl;
+
+    int  *p_copy = p_a;
+    delete[] (p_a);
+    cout << p_copy[1] <<endl;
+
+    p_copy[1] = 2;
+    cout << p_copy[1] <<endl;
+     
 }
 
 int main()
